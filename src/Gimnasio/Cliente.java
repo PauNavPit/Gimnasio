6b4dc67d8ee196cf05eac01ba9cf486778db7753
pnavarrop <paunavpit@alu.edu.gva.es>
2024-12-19 18:34:16 +0100

    Version1.0

---
+++ src/Gimnasio/Cliente.java
@@ -0,0 +1,12 @@
+package Gimnasio;
+
+import java.time.LocalDate;
+
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private LocalDate facha_nacimiento;
+    private String sexo;
+    private Peso[] historico;
+
+}
---
+++ src/Gimnasio/Peso.java
@@ -0,0 +1,19 @@
+package Gimnasio;
+
+import java.time.LocalDate;
+
+public class Peso {
+
+    private double peso;
+    private LocalDate fecha_hoy;
+    private double peso_ajustado;
+    private double metabolico_basal;
+
+
+
+
+
+}
+
+
+
